62f616ce9b5d62e6a2569df81254d2496fc703a7
koengabriels <koeng@appsaloon.be>
2019-09-02 13:53:34 +0200

    Adds Get all Badges and get badge by id call for OBF api

--- src/external-apis/Open_Badge_Factory_Api.php
+++ src/external-apis/Open_Badge_Factory_Api.php
@@ -6,6 +6,8 @@ class Open_Badge_Factory_Api {
 	// WP AJAX actions
 	const REFRESH_OBF_API_CREDENTIALS_ACTION = 'refresh_obf_api_credentials';
 	const TEST_OBF_API_CONNECTION_ACTION = 'test_obf_api_connection';
+	const OBF_GET_ALL_BADGES = 'obf_get_all_badges';
+	const OBF_GET_BADGE_BY_ID = 'obf_get_badge_by_id';
 
     // OpenBadgeFactory URLS
 	const OBF_PUBLIC_CERTIFICATE_URL = 'https://openbadgefactory.com/v1/client/OBF.rsa.pub';
@@ -13,18 +15,29 @@ class Open_Badge_Factory_Api {
     const OBF_ROUTE_SUFFIX_FOR_CSR_REQUEST_SIGNING = '/sign_request';
 
     const OBF_TEST_CONNECTION_URL = 'https://openbadgefactory.com/v1/ping/';
+    const OBF_BADGE_OPERATION_URL = 'https://openbadgefactory.com/v1/badge/';
 
     protected $credentials;
 
     public function __construct( Open_Badge_Factory_Credentials $credentials_object ) {
 		$this->credentials = $credentials_object;
+		$this->add_actions_for_ajax_calls();
+	}
 
+	private function add_actions_for_ajax_calls() {
 		add_action( 'wp_ajax_' . static::REFRESH_OBF_API_CREDENTIALS_ACTION,
 			array( $this, 'refresh_obf_api_credentials' ) );
 
 		add_action( 'wp_ajax_' . static::TEST_OBF_API_CONNECTION_ACTION,
 			array( $this, 'test_connection' )
 		);
+
+
+		add_action( 'wp_ajax_nopriv_' . static::OBF_GET_ALL_BADGES, array( $this, 'get_all_badges' ) );
+		add_action( 'wp_ajax_' . static::OBF_GET_ALL_BADGES, array( $this, 'get_all_badges' ) );
+
+		add_action( 'wp_ajax_nopriv_' . static::OBF_GET_BADGE_BY_ID, array( $this, 'get_badge_by_id' ) );
+		add_action( 'wp_ajax_' . static::OBF_GET_BADGE_BY_ID, array( $this, 'get_badge_by_id' ) );
 	}
 
 	public function refresh_obf_api_credentials() {
@@ -166,30 +179,10 @@ class Open_Badge_Factory_Api {
 
     public function test_connection() {
 		if( current_user_can( 'manage_options' ) ) {
+			$result =
+				$this->make_api_request( static::OBF_TEST_CONNECTION_URL . $this->credentials->get_client_id() );
 
-			$ch = curl_init();
-
-			$options = array(
-				CURLOPT_RETURNTRANSFER => true,
-				CURLOPT_SSL_VERIFYHOST => 2,
-				CURLOPT_SSL_VERIFYPEER => true,
-				CURLOPT_HEADER => false,
-
-				CURLOPT_URL => static::OBF_TEST_CONNECTION_URL . $this->credentials->get_client_id(),
-				CURLOPT_SSLCERT => $this->credentials->get_certificate_path(),
-				CURLOPT_SSLKEY => $this->credentials->get_private_key_path(),
-			);
-
-			curl_setopt_array($ch, $options);
-
-			$result = curl_exec($ch);
-			$info = curl_getinfo($ch);
-
-			curl_close($ch);
-
-			
-
-			if( $info['http_code'] == 200 && $result == $this->credentials->get_client_id() ) {
+			if( $result['http_code'] == 200 && $result['data'] == $this->credentials->get_client_id() ) {
 				wp_send_json_success( array() );
 			} else {
 				wp_send_json_error( array() );
@@ -200,5 +193,72 @@ class Open_Badge_Factory_Api {
 				403
 			);
 		}
+	}
+
+	public function get_all_badges() {
+		$result = $this->make_api_request( static::OBF_BADGE_OPERATION_URL . $this->credentials->get_client_id() );
+		$this->send_json_response( $result );
+	}
+
+	public function get_badge_by_id() {
+    	if( ! isset( $_POST['badge_id'] ) || strlen( $_POST['badge_id'] ) == 0 ) {
+    		wp_send_json_error( 'no badge_id given', 400 );
+		}
+
+    	$result = $this->make_api_request(
+    		static::OBF_BADGE_OPERATION_URL . $this->credentials->get_client_id() .
+			DIRECTORY_SEPARATOR . $_POST['badge_id']
+		);
+
+    	$this->send_json_response( $result, false );
+	}
+
+	private function send_json_response( $result, $send_as_array = true ) {
+		if( $result['http_code'] != 200 ) {
+			wp_send_json_error( $result['data'], $result['http_code'] );
+		} else {
+			if( $send_as_array ) {
+				wp_send_json_success( $this->convert_ldjson_to_array( $result['data'] ) );
+			} else {
+				wp_send_json_success( json_decode( $result['data'] ) ) ;
+			}
+		}
+	}
+
+	private function convert_ldjson_to_array($ldjson ) {
+		$lines = explode( PHP_EOL, $ldjson );
+		foreach( $lines as $key => $value ) {
+			if( strlen( $lines[$key] ) != 0 ) {
+				$lines[$key] = json_decode( $value, true );
+			} else {
+				unset( $lines[$key] );
+			}
+		}
+
+		return $lines;
+	}
+
+	private function make_api_request( $url ) {
+		$ch = curl_init();
+
+		$options = array(
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_SSL_VERIFYHOST => 2,
+			CURLOPT_SSL_VERIFYPEER => true,
+			CURLOPT_HEADER => false,
+
+			CURLOPT_URL => $url,
+			CURLOPT_SSLCERT => $this->credentials->get_certificate_path(),
+			CURLOPT_SSLKEY => $this->credentials->get_private_key_path(),
+		);
+
+		curl_setopt_array($ch, $options);
+
+		$result = curl_exec($ch);
+		$info = curl_getinfo($ch);
+
+		curl_close($ch);
+
+		return array( 'data' => $result, 'http_code' => $info['http_code'] );
 	}
 }
