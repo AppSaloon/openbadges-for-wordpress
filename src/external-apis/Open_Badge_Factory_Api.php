4b33e9756c75afe7c42af984a2a747953927070f
koengabriels <koeng@appsaloon.be>
2019-10-24 14:18:00 +0200

    Completes docblocks for Open_Badge_Factory_Api

--- src/external-apis/Open_Badge_Factory_Api.php
+++ src/external-apis/Open_Badge_Factory_Api.php
@@ -1,36 +1,116 @@
 <?php
-
+/**
+ * Contains the Open_Badge_Factory_Api class.
+ */
 namespace appsaloon\obwp\external_apis\openbadgefactory;
 
+/**
+ * Class Open_Badge_Factory_Api
+ *
+ * Used to communicate with the OpenBadgeFactory api.
+ * Provides a layer between WP AJAX calls and the OpenBadgeFactory api.
+ *
+ * @package appsaloon\obwp\external_apis\openbadgefactory
+ */
 class Open_Badge_Factory_Api {
-	// WP AJAX actions
+	/**
+	 * Value for action parameter for the ajax call to regenerate/refresh the credential files
+	 *
+	 */
 	const REFRESH_OBF_API_CREDENTIALS_ACTION = 'refresh_obf_api_credentials';
+
+	/**
+	 * Value for action parameter for the ajax call to test the connection to the OpenBadgeFactory api
+	 *
+	 */
 	const TEST_OBF_API_CONNECTION_ACTION = 'test_obf_api_connection';
+
+	/**
+	 * Value for action parameter for the ajax call to get all badges from the OpenBadgeFactory api
+	 *
+	 */
 	const OBF_GET_ALL_BADGES = 'obf_get_all_badges';
+
+	/**
+	 * Value for action parameter for the ajax call to a badge by id from the OpenBadgeFactory api
+	 *
+	 */
 	const OBF_GET_BADGE_BY_ID = 'obf_get_badge_by_id';
+
+	/**
+	 * Value for action parameter for the ajax call to issue a badge from the OpenBadgeFactory api
+	 *
+	 */
 	const OBF_ISSUE_BADGE = 'obf_issue_badge';
 
-    // OpenBadgeFactory URLS
+	/**
+	 * Url to the public certificate provided by OpenBadgeFactory, used to generate credential files
+	 *
+	 */
 	const OBF_PUBLIC_CERTIFICATE_URL = 'https://openbadgefactory.com/v1/client/OBF.rsa.pub';
-    const OBF_BASE_URL_FOR_SIGNING_CLIENT_CERTIFICATE_REQUEST = 'https://openbadgefactory.com/v1/client/';
-    const OBF_ROUTE_SUFFIX_FOR_CSR_REQUEST_SIGNING = '/sign_request';
 
-    const OBF_TEST_CONNECTION_URL = 'https://openbadgefactory.com/v1/ping/';
-    const OBF_BADGE_OPERATION_URL = 'https://openbadgefactory.com/v1/badge/';
+	/**
+	 * Url to sign the request for a client certificate
+	 *
+	 */
+	const OBF_BASE_URL_FOR_SIGNING_CLIENT_CERTIFICATE_REQUEST = 'https://openbadgefactory.com/v1/client/';
 
-    protected $credentials;
-    protected $plugin_url;
+	/**
+	 * Suffix to append to the OBF_BASE_URL_FOR_SIGNING_CLIENT_CERTIFICATE_REQUEST to sign
+	 * the client certificate request.
+	 *
+	 */
+	const OBF_ROUTE_SUFFIX_FOR_CSR_REQUEST_SIGNING = '/sign_request';
 
-    public function __construct( Open_Badge_Factory_Credentials $credentials_object, $plugin_url ) {
+	/**
+	 * Url to the test the connection to the OpenBadgeFactory api (testing the validity of the credentials files).
+	 *
+	 */
+	const OBF_TEST_CONNECTION_URL = 'https://openbadgefactory.com/v1/ping/';
+
+	/**
+	 * Url to do badge operations with the OpenBadgeFactory api.
+	 *
+	 */
+	const OBF_BADGE_OPERATION_URL = 'https://openbadgefactory.com/v1/badge/';
+
+	/**
+	 * Open_Badge_Factory_Credentials instance used to sign the requests.
+	 *
+	 * @var Open_Badge_Factory_Credentials
+	 *
+	 */
+	protected $credentials;
+
+	/**
+	 * Url to the plugin main directory
+	 * @var string
+	 *
+	 */
+	protected $plugin_url;
+
+	/**
+	 * Open_Badge_Factory_Api constructor.
+	 * @param Open_Badge_Factory_Credentials $credentials_object
+	 * @param string $plugin_url
+	 *
+	 * @since 1.0.5
+	 */
+	public function __construct( Open_Badge_Factory_Credentials $credentials_object, $plugin_url ) {
     $this->plugin_url = $plugin_url;
 		$this->credentials = $credentials_object;
 		$this->add_actions_for_ajax_calls();
 		$this->add_shortcodes();
 	}
 
+	/**
+	 * Adds all the action hooks to expose the OpenBadgeFactory api through WP ajax calls
+	 *
+	 * @since 1.0.5
+	 */
 	private function add_actions_for_ajax_calls() {
 		add_action( 'wp_ajax_' . static::REFRESH_OBF_API_CREDENTIALS_ACTION,
-			array( $this, 'refresh_obf_api_credentials' ) );
+			array( $this, 'ajax_refresh_obf_api_credentials' ) );
 
 		add_action( 'wp_ajax_' . static::TEST_OBF_API_CONNECTION_ACTION,
 			array( $this, 'ajax_test_connection' )
@@ -47,11 +127,29 @@ class Open_Badge_Factory_Api {
 		add_action( 'wp_ajax_' . static::OBF_ISSUE_BADGE, array( $this, 'ajax_issue_badge' ) );
 	}
 
+	/**
+	 * Adds all the shortcodes that make use of the OpenBadgeFactory api.
+	 *
+	 * @since 1.0.5
+	 */
 	private function add_shortcodes() {
+		// TODO: set tag in a constant
     	add_shortcode( 'obf_display_claimable_badge', array( $this, 'display_claimable_badge' ) );
 	}
 
+	/**
+	 * Loads the template to display a claimable badge if the current user can claim it.
+	 * Returns either the template or an error message as a string or false.
+	 *
+	 * Parameter $atts should be an array with index 'id' and value is the id of the badge to display.
+	 *
+	 * @param array $atts
+	 * @return false|string
+	 *
+	 * @since 1.0.5
+	 */
 	public function display_claimable_badge( $atts ) {
+		// TODO only run logic to check if a user can claim a badge when the badge can be retrieved
     	$is_user_allowed_to_claim_badge = is_user_logged_in();
 
     	if( $is_user_allowed_to_claim_badge ) {
@@ -60,6 +158,7 @@ class Open_Badge_Factory_Api {
     		$user_email = '';
 		}
 
+    	//TODO move enqueueing to separate private function
 		wp_enqueue_style('openbadge-css', $this->plugin_url .'files/css/openbadges.css');
 		wp_enqueue_script('openbadges-js', $this->plugin_url .'files/js/openbadges.js', 'jquery' , '' , true );
 		wp_localize_script('openbadges-js', 'openbadges_ajax_object', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
@@ -71,7 +170,22 @@ class Open_Badge_Factory_Api {
 		}
 	}
 
-	private function load_claimable_badge_template( $is_user_allowed_to_claim_badge, $user_email, $badge) {
+	/**
+	 * Loads the template for displaying a claimable badge.
+	 * Parameters are necessary to make them available to the template.
+	 * Allows themes to override the template.
+	 *
+	 * Uses output buffering because it serves a template to a shortcode.
+	 *
+	 * @param boolean $is_user_allowed_to_claim_badge
+	 * @param string $user_email
+	 * @param array $badge
+	 *
+	 * @return false|string
+	 *
+	 * @since 1.0.5
+	 */
+	private function load_claimable_badge_template($is_user_allowed_to_claim_badge, $user_email, $badge) {
 		ob_start();
 		$template_from_theme = locate_template( 'openbadges-for-wordpress/open-badge-claim.php' );
 		if( '' !== $template_from_theme  ){
@@ -82,7 +196,14 @@ class Open_Badge_Factory_Api {
 		return ob_get_clean();
 	}
 
-	public function refresh_obf_api_credentials() {
+	/**
+	 * Handles the WP ajax request for action REFRESH_OBF_API_CREDENTIALS_ACTION.
+	 * Upon success it saves new credential files and sends a json success response.
+	 * On failure it returns the appropriate json error response.
+	 *
+	 * @since 1.0.5
+	 */
+	public function ajax_refresh_obf_api_credentials() {
 		$this->send_error_for_invalid_nonce();
 
 		if( current_user_can( 'manage_options' ) ) {
@@ -132,7 +253,14 @@ class Open_Badge_Factory_Api {
 		}
 	}
 
+	/**
+	 * Checks the security nonce when requesting new credentials.
+	 * Sends a json error message if the nonce is invalid, returns void when nonce is valid.
+	 *
+	 * @since 1.0.5
+	 */
 	private function send_error_for_invalid_nonce() {
+		//TODO set value for action parameter to a class constant
 		$is_nonce_valid = check_ajax_referer( 'configuration', 'security', false);
 
 		if( ! $is_nonce_valid ) {
@@ -140,6 +268,13 @@ class Open_Badge_Factory_Api {
 		}
 	}
 
+	/**
+	 * Returns true if the request to refresh the credentials was valid, false if it was not.
+	 *
+	 * @return bool
+	 *
+	 * @since 1.0.5
+	 */
 	private function is_valid_refresh_obf_api_credentials_request() {
 		if( empty( $_POST ) ) {
 			return false;
@@ -152,11 +287,31 @@ class Open_Badge_Factory_Api {
 		}
 	}
 
+	/**
+	 * Returns the $credentials class property
+	 *
+	 * @return Open_Badge_Factory_Credentials
+	 *
+	 * @since 1.0.5
+	 */
 	public function get_credentials() {
     	return $this->credentials;
 	}
 
+	/**
+	 * Generates a matching client certificate and private key pair based on the api token.
+	 * Sends a private key and a certificate signing request (CSR) to the OpenBadgeFactory api to generate
+	 * the client certificate.
+	 *
+	 * @param string $api_token
+	 *
+	 * @return array|string
+	 *
+	 * @since 1.0.5
+	 */
 	private function generate_client_certificate_private_key_pair( $api_token ) {
+		//TODO extract private functions to increase readability
+		// TODO maybe change return type string?
         if( current_user_can( 'manage_options' ) ) {
             $obf_public_certificate = wp_remote_get( static::OBF_PUBLIC_CERTIFICATE_URL );
 
@@ -219,7 +374,13 @@ class Open_Badge_Factory_Api {
         }
     }
 
-    public function ajax_test_connection() {
+	/**
+	 * Handles the WP Ajax request to check if the credential files are valid
+	 * Sends a wp json success if credentials are valid, wp json error if not.
+	 *
+	 * @since 1.0.5
+	 */
+	public function ajax_test_connection() {
 		if( current_user_can( 'manage_options' ) ) {
 			$result =
 				$this->make_api_request( static::OBF_TEST_CONNECTION_URL . $this->credentials->get_client_id() );
@@ -237,11 +398,23 @@ class Open_Badge_Factory_Api {
 		}
 	}
 
+	/**
+	 * Handles the WP Ajax request to retrieve all badges from OpenBadgeFactory api.
+	 * Sends a json response.
+	 *
+	 * @since 1.0.5
+	 */
 	public function ajax_get_all_badges() {
 		$result = $this->make_api_request( static::OBF_BADGE_OPERATION_URL . $this->credentials->get_client_id() );
 		$this->send_json_response( $result );
 	}
 
+	/**
+	 * Handles the WP Ajax request to retrieve a badge by id from OpenBadgeFactory api.
+	 * Sends a json response containing the badge data or json error response.
+	 *
+	 * @since 1.0.5
+	 */
 	public function ajax_get_badge_by_id() {
     	if( ! isset( $_POST['badge_id'] ) || strlen( $_POST['badge_id'] ) == 0 ) {
     		wp_send_json_error( 'no badge_id given', 400 );
@@ -255,6 +428,12 @@ class Open_Badge_Factory_Api {
     	$this->send_json_response( $result, false );
 	}
 
+	/**
+	 * Handles the WP Ajax request to issue a badge.
+	 * Sends a json success response or error.
+	 *
+	 * @since 1.0.5
+	 */
 	public function ajax_issue_badge() {
     	// TODO: check security nonce!!
 		if( ! isset( $_POST['badge_id'] ) || strlen( $_POST['badge_id'] ) == 0 ) {
@@ -281,6 +460,17 @@ class Open_Badge_Factory_Api {
 		}
 	}
 
+	/**
+	 * Gets a badge by id.
+	 * Returns badge data as array or false
+	 *
+	 * @param $badge_id
+	 * @return array|bool
+	 *
+	 * TODO determine badge_id type
+	 *
+	 * @since 1.0.5
+	 */
 	private function get_badge_by_id($badge_id ) {
 		$result = $this->make_api_request(
 			static::OBF_BADGE_OPERATION_URL . $this->credentials->get_client_id() .
@@ -294,7 +484,15 @@ class Open_Badge_Factory_Api {
 		}
 	}
 
-	private function send_json_response( $result, $send_as_array = true ) {
+	/**
+	 * Sends the appropriate json response with, converts data in parameter $result as needed for response.
+	 *
+	 * @param array $result
+	 * @param bool $send_as_array
+	 *
+	 * @since 1.0.5
+	 */
+	private function send_json_response($result, $send_as_array = true ) {
 		if( $result['http_code'] != 200 ) {
 			if( $result['http_code'] == 201 ) {
 				wp_send_json_success( $result['data'] );
@@ -310,6 +508,14 @@ class Open_Badge_Factory_Api {
 		}
 	}
 
+	/**
+	 * Converts a line delimited json string to an array and returns the array.
+	 *
+	 * @param string $ldjson
+	 * @return array
+	 *
+	 * @since 1.0.5
+	 */
 	private function convert_ldjson_to_array($ldjson ) {
 		$lines = explode( PHP_EOL, $ldjson );
 		foreach( $lines as $key => $value ) {
@@ -323,7 +529,19 @@ class Open_Badge_Factory_Api {
 		return $lines;
 	}
 
-	private function make_api_request( $url, $body = array() ) {
+	/**
+	 * Makes an api request to $url with $body as post, performs get request when $body is empty (default).
+	 * Returns an index array, index 'data' contains the result and index 'http_code' contains the http response code
+	 * from the request.
+	 *
+	 * @param string $url
+	 * @param array $body
+	 *
+	 * @return array
+	 *
+	 * @since 1.0.5
+	 */
+	private function make_api_request($url, $body = array() ) {
 		$ch = curl_init();
 
 		$options = array(
